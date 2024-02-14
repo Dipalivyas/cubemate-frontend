b23998a628a118a00872ddcdbc80fc240089a828
Vedant <vedant@swamiinfotech.in>
2024-02-14 17:10:34 +0530

    job-details design complete

--- src/app/JobDetails/view/job-details/job-details.component.ts
+++ src/app/JobDetails/view/job-details/job-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-job-details',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class JobDetailsComponent {
 
+
+  isClicked: boolean = false;
+  customOptions3: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 600,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      760: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    },
+    nav: false
+  }
 }
