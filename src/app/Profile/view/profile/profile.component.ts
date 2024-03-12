35e633e0b3b9f0c9e31b310414e4dc3b7050766b
Vedant <vedant@swamiinfotech.in>
2024-03-12 11:18:16 +0530

    changes

--- src/app/Profile/view/profile/profile.component.ts
+++ src/app/Profile/view/profile/profile.component.ts
@@ -111,57 +111,57 @@ export class ProfileComponent implements OnInit{
 
   image:any;
 
-  uploadImg(element: any) {
-
-    var file = element.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e: any) => {      
-      this.image=e.target.result;
-      let data: string = e.target.result;
-      //let extension:string=data.substring("data:image/".length, data.indexOf(";base64"))
-      let data2: string = data.substring(data.indexOf('|') + 1);
-      let base64 = data2.substring(data2.indexOf(',') + 1);
-      this.med.mediaFileBase64 = base64;
-      // this.themes.imageextention = extension;
-      this.file.push(this.med.mediaFileBase64);
-      // this.ext.push(this.themes.imageextention);
-    };
-    reader.readAsDataURL(file);
-  }
-
   // uploadImg(element: any) {
-  //   // Get the file from the input
-  //   const file = element.target.files[0];
+
+  //   var file = element.target.files[0];
   //   const reader = new FileReader();
-  
-  //   reader.onload = (e: any) => {
-  //     // e.target.result contains the Base64 DataURL
-  //     const dataURL: string = e.target.result;
-  
-  //     // Extract the Base64 encoded data, discard the DataURL metadata
-  //     const base64Data = dataURL.split(',')[1];
-  
-  //     // Optionally, extract MIME type as well
-  //     const mimeType = dataURL.split(',')[0].split(':')[1].split(';')[0];
-  
-  //     // Store the base64 encoded data in your model, assuming you want to use it for saving or display purposes
-  //     this.med.mediaFileBase64 = base64Data;
-  
-  //     // If needed, store the MIME type as well for future reference or use
-  //     // this.med.mediaType = mimeType;
-  
-  //     // Handling multiple files, if 'this.file' is an array intended to store multiple files' Base64 strings
-  //     this.file.push(base64Data); // Storing the base64 encoded data
-  //     // this.mimeTypes.push(mimeType); // If you're keeping track of MIME types as well
-  
-  //     // Debug or verify the file's MIME type and base64 data
-  //     console.log('File MIME type:', mimeType);
-  //     console.log('Base64 Data:', base64Data);
+  //   reader.onload = (e: any) => {      
+  //     this.image=e.target.result;
+  //     let data: string = e.target.result;
+  //     //let extension:string=data.substring("data:image/".length, data.indexOf(";base64"))
+  //     let data2: string = data.substring(data.indexOf('|') + 1);
+  //     let base64 = data2.substring(data2.indexOf(',') + 1);
+  //     this.med.mediaFileBase64 = base64;
+  //     // this.themes.imageextention = extension;
+  //     this.file.push(this.med.mediaFileBase64);
+  //     // this.ext.push(this.themes.imageextention);
   //   };
-  
-  //   // Read the file as a DataURL, works for any file type
   //   reader.readAsDataURL(file);
   // }
+
+  uploadImg(element: any) {
+    // Get the file from the input
+    const file = element.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = (e: any) => {
+      // e.target.result contains the Base64 DataURL
+      const dataURL: string = e.target.result;
+  
+      // Extract the Base64 encoded data, discard the DataURL metadata
+      const base64Data = dataURL.split(',')[1];
+  
+      // Optionally, extract MIME type as well
+      const mimeType = dataURL.split(',')[0].split(':')[1].split(';')[0];
+  
+      // Store the base64 encoded data in your model, assuming you want to use it for saving or display purposes
+      this.med.mediaFileBase64 = base64Data;
+  
+      // If needed, store the MIME type as well for future reference or use
+      // this.med.mediaType = mimeType;
+  
+      // Handling multiple files, if 'this.file' is an array intended to store multiple files' Base64 strings
+      this.file.push(base64Data); // Storing the base64 encoded data
+      // this.mimeTypes.push(mimeType); // If you're keeping track of MIME types as well
+  
+      // Debug or verify the file's MIME type and base64 data
+      console.log('File MIME type:', mimeType);
+      console.log('Base64 Data:', base64Data);
+    };
+  
+    // Read the file as a DataURL, works for any file type
+    reader.readAsDataURL(file);
+  }
   
 
 
